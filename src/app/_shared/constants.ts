73ae2536c488568fc27a69af462f6afe1d65a1cd
gondelcorro <gonzalo_delcorro@hotamail.com>
2019-03-12 13:23:47 -0300

    Commit animation card

--- src/app/_shared/constants.ts
+++ src/app/_shared/constants.ts
@@ -1,2 +1,2 @@
-//export const HOST = "http://localhost:8080";
-export const HOST = "http://167.99.172.40/MovieApp-backend";
+export const HOST = "http://localhost:8080";
+//export const HOST = "http://167.99.172.40/MovieApp-backend";
